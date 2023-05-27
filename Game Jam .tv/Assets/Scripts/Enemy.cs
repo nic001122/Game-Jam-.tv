029804b78bd9ab6fae958ce9f7522d61fd5c3240
nic001122 <123776405+nic001122@users.noreply.github.com>
2023-05-27 23:20:48 +0200

    added sword

---
+++ Game Jam .tv/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 10;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
---
+++ Game Jam .tv/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask whatisEnemies;
+    public int damage = 5;
+
+    public void Update()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatisEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                   enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+            
+            timeBtwAttack = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
