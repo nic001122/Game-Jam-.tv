6e86266e0b4974480e3a6e7f5844f486407a4488
NewByList <tonatiuh@gmx.de>
2023-05-28 10:27:18 +0200

    finished flower

--- Game Jam .tv/Assets/Scripts/FlowerController.cs
+++ Game Jam .tv/Assets/Scripts/FlowerController.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FlowerController : MonoBehaviour
 {
-    
+    public bool pickedUpFlower;
+
+    public void PickUpFlower()
+    {
+        if (!pickedUpFlower)
+        {
+            pickedUpFlower = true;
+            SceneManager.LoadScene(2);
+        }
+    }
 }
