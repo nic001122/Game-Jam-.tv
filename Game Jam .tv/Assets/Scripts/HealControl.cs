0faec484437805aa3ae4bef46e33185c4504f497
NewByList <tonatiuh@gmx.de>
2023-05-28 12:26:34 +0200

    fixed health bar on heal

--- Game Jam .tv/Assets/Scripts/HealControl.cs
+++ Game Jam .tv/Assets/Scripts/HealControl.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class HealControl : MonoBehaviour
 {
     public Player player;
+    public HealthBar healthBar;
     // Start is called before the first frame update
     public void Heal()
     {
         player.currentHealth = player.maxHealth;
+        healthBar.SetMaxHealth(player.maxHealth);
         Destroy(gameObject);
     }
 }
